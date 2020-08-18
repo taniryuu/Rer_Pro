a5e65640b56de5d0801554e9506e7637882300e4
taniryuu <tr76aquarius@gmail.com>
2020-08-18 16:49:23 +0900

    seedファイルにシステムサイドのアカウントを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Company.create!(
+  name: "システム管理",
+  admin: true,
+  status: 0
+)
+puts "システム管理者作成完了"
+
+# Company.create!(
+#   name: "システム管理",
+#   admin: true,
+#   status: 0
+# )
