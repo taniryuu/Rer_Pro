3df8f5333ed9d99b5d40adf8ab91571992767efd
taniryuu <tr76aquarius@gmail.com>
2020-08-19 12:28:10 +0900

    deviseを使用してuserのMVC作成、routes.rbを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  resources :companies
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks',
+  }
+  resources :companies do
+    resources :users
+  end
 end
