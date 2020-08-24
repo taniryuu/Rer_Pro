9dbe9281c385c00dcacf05b465f6801528a4f86e
githubで登録している名前 <tr76aquarius@gmail.com>
2020-08-24 12:30:17 +0900

    deviseの日本語化設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :leads
+  end
   resources :companies
-  resources :leads
 end
