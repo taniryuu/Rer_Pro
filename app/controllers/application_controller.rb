e325cdc918c35cae3443e42e67101a8e9921f8ef
githubで登録している名前 <tr76aquarius@gmail.com>
2020-08-24 17:00:20 +0900

    ログイン、ログアウト機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
+  def after_sign_in_path_for(resource)
+    if current_user
+        user_path(resource)
+      else
+        root_path
+      end
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
   protected
 
     def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+    post 'login' => 'devise/sessions#create', as: :user_session
+    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
+  devise_for :users, skip: [:sessions]
   resources :users, only: [:index, :show] do
     resources :leads
   end
