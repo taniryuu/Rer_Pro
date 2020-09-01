3e19f36d9ae4c873360ae029d53f01d5c5e62ba3
ruffini47 <ruffini47@gmail.com>
2020-09-01 12:00:43 +0000

    Set enum status in Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { in: 0..60 }
   validates :memo, length: { in: 0..400 }
   validates :scheduled_complete_date, presence: true
+  enum status: [:not_yet, :completed, :canceled] # 進捗ステータス
 end
