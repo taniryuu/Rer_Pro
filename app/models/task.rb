fd727efa7614000c36ca5c61bba1c3575cc106f1
ruffini47 <ruffini47@gmail.com>
2020-09-01 11:08:14 +0000

    set presence :true name and scheduled_complete_date column

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :step
-  validates :name, length: { in: 0..60 }
+  validates :name, presence: true, length: { in: 0..60 }
   validates :memo, length: { in: 0..400 }
+  validates :scheduled_complete_date, presence: true
 end
