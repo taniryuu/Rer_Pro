32d94486fcf55e95857040ee97ffadecb3bbf877
githubで登録している名前 <tr76aquarius@gmail.com>
2020-09-02 10:06:37 +0900

    deviseのauthenticate_userを切り出した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
 
   # ユーザーIDを取得し識別
   def set_user
@@ -28,9 +27,9 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
-  # 管理者かどうか識別し管理者以外ならtopに戻しflash表示
+  # ログインユーザーを管理者かどうか識別し管理者以外ならtopに戻しflash表示
   def current_user_admin?
-    unless current_user.admin? || user_signed_in?
+    if user_signed_in? && current_user.admin?
       redirect_to root
       flash[:danger] = "アクセスが無効です。"
     end
@@ -38,8 +37,8 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :login_id, :superior, :admin, :superior_id, :company_id])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :login_id, :status, :superior, :admin, :superior_id, :company_id])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:login_id])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :superior, :superior_id, :email, :notified_num, :password, :password_confirmation, :current_password])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :superior, :status, :superior_id, :email, :notified_num, :password, :password_confirmation, :current_password])
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
-class UsersController < ApplicationController
+class UsersController < Users::ApplicationController
   before_action :set_user, only: %i(show edit destroy)
   before_action :set_members, only: %i(index edit)
+  before_action :set_company_id, only: :new
 
   def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user.create!(user_params)
+      flash[:success] = "登録に成功しました"
+      log_in @user
+      redirect_to user_url
+    else
+      flash[:danger] = "登録に失敗しました"
+    end
   end
 
   def show
@@ -16,4 +31,10 @@ class UsersController < ApplicationController
     @user.destroy ? flash[:success] : flash[:danger]
     redirect_to users_url
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :login_id, :superior, :email, :password, :password_confirmation, :company_id, :superior_id)
+    end
 end
