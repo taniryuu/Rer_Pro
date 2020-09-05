2a97b69ec992650f2d6053472f31355027a747e3
githubで登録している名前 <tr76aquarius@gmail.com>
2020-09-05 09:54:25 +0900

    ページ遷移しやすいよう微調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < Users::ApplicationController
-  before_action :set_user, only: %i(show edit destroy)
-  before_action :set_members, only: %i(index edit)
+  before_action :set_user, only: %i(show destroy)
+  before_action :set_members, only: %i(index)
 
   def index
   end
@@ -15,15 +15,12 @@ class UsersController < Users::ApplicationController
       flash[:success] = "登録に成功しました"
       redirect_to users_url
     else
-      flash[:danger] = "登録に失敗しました"
+      flash.now[:danger] = "登録に失敗しました"
       render :new
     end
   end
 
   def show
-  end
-
-  def edit
   end
 
   def destroy
