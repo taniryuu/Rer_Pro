89296ceb61f70c6dfbddf375a0ce53c625fcd29b
ruffini47 <ruffini47@gmail.com>
2020-09-10 08:25:34 +0000

    Delete-function-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -63,34 +63,22 @@ class TasksController < StepsController
     n = checkbox_array.size
     n.times do |i|
       if checkbox_array[i] == "true"
-        checkbox_array.delete_at(i-1)
+        checkbox_array.delete_at(i - 1)
       end
     end
-    #debugger
     n1 = checkbox_array.size
-    deleted_tasks = []
+    #deleted_tasks = []
     i2 = 0
     n1.times do |i1|
       if checkbox_array[i1] == "true"
-        deleted_tasks[i2] = @step.tasks.where(status: "not_yet").order(:scheduled_complete_date).limit(1).offset(i1-i2)
-        debugger
+        deleted_tasks = @step.tasks.where(status: "not_yet").order(:scheduled_complete_date).limit(1).offset(i1 - i2)
         i2 += 1
-        #deleted_tasks.each do |deleted_task|
-        #  deleted_task.update_attribute(:status, "completed")
-        #  debugger
-        #end
+        deleted_tasks.each do |deleted_task|
+          deleted_task.update_attribute(:status, "completed")
+        end
       end
     end
-    debugger
-    i2.times do |i3|
-      debugger
-      deleted_tasks[i3].each do |deleted_task|
-        debugger
-        deleted_task.update_attribute(:status, "completed");
-      end
-    end
-
-    #redirect_to action: 'index', :lead_id => 1, :step_id => 1
+    
     redirect_to lead_step_tasks_url(@lead, @step)
   end
 
