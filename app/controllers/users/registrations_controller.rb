011f1bfc2ab296fc2bcd8a39a7b9938f707dc4f2
githubで登録している名前 <tr76aquarius@gmail.com>
2020-09-20 22:00:07 +0900

    Company#createを修正,エラーメッセージ対応

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,7 +5,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   prepend_before_action :set_minimum_password_length, only: %i(edit)
   before_action :set_members, only: %i(edit update)
   before_action :same_company_id, only: %i(edit update)
-  # before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -79,11 +78,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   end
@@ -97,15 +91,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     user_signed_in? && current_user.admin?
   end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   if current_user_is_admin?
-  #     users_path
-  #   else
-  #     super(resource)
-  #   end
-  # end
-
   # The path used after update.
   def after_update_path_for(resource)
     if current_user_is_admin?
@@ -115,17 +100,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # def sign_up(resource_name, resource)
-  #   if !current_user_is_admin?
-  #     sign_in(resource_name, resource)
-  #   end
-  # end
-  
   def update_resource_without_password(resource, params)
     resource.update_without_password(params)
   end
