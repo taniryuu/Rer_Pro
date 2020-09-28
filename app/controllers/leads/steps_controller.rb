8220e89e01b2e0900a95f7844380c8115bf9892b
fumihisa suzuki <fumi_s_qrio192@yahoo.co.jp>
2020-09-28 23:59:55 +0000

    Create layout-outline of Lead Show Page

--- app/controllers/leads/steps_controller.rb
+++ app/controllers/leads/steps_controller.rb
@@ -12,14 +12,15 @@ class Leads::StepsController < Leads::ApplicationController
   # GET /steps
   # GET /steps.json
   def index
-    @steps = @lead.steps.all
+    @steps = @lead.steps.all.order(:order)
   end
 
   # GET /steps/1
   # GET /steps/1.json
   def show
-    @steps_except_self = @lead.steps.where.not(id: @step.id)
-    @steps_from_now_on = @steps_except_self.where(status: ["not_yet", "in_progress"])
+    @steps = @lead.steps.all.order(:order)
+    @steps_except_self = @lead.steps.where.not(id: @step.id).order(:order)
+    @steps_from_now_on = @steps_except_self.where(status: ["not_yet", "in_progress"]).order(:order)
   end
 
   # GET /steps/new
