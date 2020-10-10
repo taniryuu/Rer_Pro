7b25b9a8fd4b004ce5fb770f1fd3353bb25df5d7
ruffini47 <ruffini47@gmail.com>
2020-10-10 07:19:21 +0000

    completed_date_prohibit_future validation setting

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,15 +3,9 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { in: 0..60 }
   validates :memo, length: { in: 0..400 }
   validates :scheduled_complete_date, presence: true
+  validate :completed_date_prohibit_future # 完了日に現在の日付より未来の日付を入れる場合はアラート
   enum status: [:not_yet, :completed, :canceled] # 進捗ステータス
   
-  # 完了日に現在の日付より未来の日付を入れる場合はアラート
-  validate :not_newer_than_today
-
-  def not_newer_than_today
-    errors.add(:completed_date, "は未来の日付は入れられません") if completed_date.present? && Date.parse(completed_date) > Date.current
-  end
-
   # 完了日または中止にした日が空なら今日の日付を入れる
   def date_blank_then_today(status)
     if status == "completed"
