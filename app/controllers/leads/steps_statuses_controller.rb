92705a5eebb3cedac1c3e79d92638f3771a57316
ruffini47 <ruffini47@gmail.com>
2020-10-18 14:21:46 +0000

    Improve redirect_to for def complete

--- app/controllers/leads/steps_statuses_controller.rb
+++ app/controllers/leads/steps_statuses_controller.rb
@@ -14,7 +14,7 @@ class Leads::StepsStatusesController < Leads::StepsController
         raise ActiveRecord::Rollback if @lead.invalid?(:check_steps_status)
       end
       if @lead.errors.blank?
-        redirect_to check_status_and_get_url(completed_step, @step)
+        redirect_to @step
       else
         flash[:danger] = "#{flash[:danger]}#{@lead.errors.full_messages.first}"
         redirect_to completed_step
