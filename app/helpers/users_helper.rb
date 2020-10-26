8a7e2d31985a05ef5fc9a3dab38dc555dcee2fa1
githubで登録している名前 <tr76aquarius@gmail.com>
2020-10-26 17:49:27 +0900

    コメント追加、インデント等修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,11 +4,6 @@ module UsersHelper
     @users.find(id).name
   end
 
-  # 建物名と部屋番号を繋げて表示する
-  def room_name_num(lead)
-    "#{lead.room_name}" + ":" + "#{lead.room_num}号室"
-  end
-
   # 管理者は削除対象にできない設定+削除対象のユーザーと削除実行ユーザーが同じ企業IDか判定
   def delete_judgment(user)
     user.admin? || user.company_id != current_user.company_id ? false : true
