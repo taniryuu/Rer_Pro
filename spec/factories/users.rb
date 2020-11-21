243d3eee4a005aee988bb0da5f8a7f3ec9bd1b42
githubで登録している名前 <tr76aquarius@gmail.com>
2020-11-21 12:22:54 +0900

    FactoryBotでUser作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,46 @@
 FactoryBot.define do
-  factory :user do
-    name { "サンプル上長" }
+  factory :admin_user do
+    name { "サンプル管理者" }
     company_id { 1 }
     password { "password" }
     login_id { "00001" }
+    superior { true }
+    admin { true }
+    superior_id { "" }
+    lead_count { 0 }
+    lead_count_delay { 0 }
+    notified_num { 3 }
+    email { "admin@example.com" }
+    status { 0 }
+  end
+
+  factory :superior_user do
+    name { "サンプル上長" }
+    company_id { 1 }
+    password { "password" }
+    login_id { "00002" }
     superior { true }
     admin { false }
     superior_id { 1 }
     lead_count { 0 }
     lead_count_delay { 0 }
     notified_num { 3 }
-    email { "sample@email.com" }
+    email { "superior@example.com" }
+    status { 0 }
+  end
+
+  factory :user do
+    name { "サンプルユーザー" }
+    company_id { 1 }
+    password { "password" }
+    login_id { "00003" }
+    superior { false }
+    admin { false }
+    superior_id { 1 }
+    lead_count { 0 }
+    lead_count_delay { 0 }
+    notified_num { 3 }
+    email { "sample@example.com" }
     status { 0 }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "companies#newで管理者登録時" do
+    describe "active_superior_in_the_same_companyメソッドのテスト" do
+      context "正しい情報かつ他にユーザーが登録されていない場合" do
+        it "登録に成功すること" do
+          
+        end
+      end
+      context "すでに管理者や他のユーザーが保存されている場合" do
+        it "登録失敗すること" do
+          
+        end
+      end
+    end
+  end
 end
