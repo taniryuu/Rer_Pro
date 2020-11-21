a149322bb5170fc1a1a3d8369558713005f654b4
fumihisa suzuki <fumi_s_qrio192@yahoo.co.jp>
2020-11-21 09:32:31 +0000

    Sent params[:template_id] to new_lead

--- app/helpers/leads/template_helper.rb
+++ app/helpers/leads/template_helper.rb
@@ -1,2 +1,13 @@
 module Leads::TemplateHelper
+  # テンプレートがある場合、日付をスライドさせる
+  def modified_date(date, template_date, date_difference)
+    if template_date.blank?
+      date
+    elsif template_date.is_a?(Date)
+      template_date + date_difference
+    else
+      Date.parse(template_date) + date_difference
+    end
+  end
+      
 end
