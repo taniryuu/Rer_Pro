6f73724543a7227416e8d479b70dc4e75fb663ac
ruffini47 <ruffini47@gmail.com>
2020-12-21 15:06:31 +0000

    タスク作成に失敗したらrenderでleads/steps/showに戻る

--- app/controllers/leads/tasks_controller.rb
+++ app/controllers/leads/tasks_controller.rb
@@ -4,6 +4,7 @@ class Leads::TasksController < Leads::ApplicationController
                                     edit_revive_from_canceled_list update_revive_from_canceled_list)
   before_action :set_step, only: %i(index new create)
   before_action :set_step_by_id, only: [:add_delete_list, :complete_all_tasks]
+  before_action :set_steps, only: :create
   # アクセス制限
   before_action :correct_user, except: %i(index show)
 
@@ -35,7 +36,9 @@ class Leads::TasksController < Leads::ApplicationController
       check_status_and_redirect_to(@step, @step, nil)
     else
       flash[:danger] = "#{@task.errors.full_messages.first}"
-      redirect_to @step
+      render :template=> "leads/steps/show", :collection => @steps, :locals=> { :@tasks=> @step.tasks.not_yet.order(:scheduled_complete_date), 
+                                                                                :@completed_tasks => @step.tasks.completed.order(:completed_date),
+                                                                                :@canceled_tasks => @step.tasks.canceled.order(:canceled_date) }
     end
   end
 
